40783877ba9f64222cc2bbb98f019b91ae8ee5c1
Mathilde Renard <mathilderenard626@gmail.com>
2022-03-21 23:18:54 +0100

    mise en place de la carte avec ville

--- superHeros/src/main/java/com/example/superheros/controller/IncidentController.java
+++ superHeros/src/main/java/com/example/superheros/controller/IncidentController.java
@@ -2,15 +2,11 @@ package com.example.superheros.controller;
 
 import java.util.List;
 
+import com.example.superheros.model.SuperHeros;
 import com.example.superheros.service.IncidentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import com.example.superheros.model.Incident;
 
@@ -29,5 +25,6 @@ public class IncidentController {
                 model.addAttribute("incidents", incidents);
                 return  "incident/index";
         }
+
 
 }
